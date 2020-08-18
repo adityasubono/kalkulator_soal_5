39131c94388ef9c918cf4dc452d76df01a00ba28
Aditya Subono <adityasubono@gmail.com>
2020-08-18 13:26:24 +0700

    Update Percent

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ResultComponent from './components/ResultComponent';
+import KeyPadComponent from "./components/KeyPadComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      result: ""
+    }
+  }
+
+  onClick = button => {
+
+    if(button === "="){
+      this.calculate()
+    }
+
+    else if(button === "C"){
+      this.reset()
+    }
+    else if(button === "%"){
+      this.percent()
+    }
+
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  };
+
+
+  calculate = () => {
+    var checkResult = ''
+    if(this.state.result.includes('--')){
+      checkResult = this.state.result.replace('--','+')
+    }
+
+    else {
+      checkResult = this.state.result
+    }
+
+    try {
+      if(this.state.result.includes('%')){
+        this.setState({
+          result: (eval(checkResult)/100 || "" ) + ""
+        })
+      }
+      this.setState({
+        result: (eval(checkResult) || "" ) + ""
+      })
+    } catch (e) {
+      this.setState({
+        result: "error"
+      })
+
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  // backspace = () => {
+  //   this.setState({
+  //     result: this.state.result.slice(0, -1)
+  //   })
+  // };
+
+  percent = () => {
+    this.setState({
+      result: this.state.result/100
+    })
+  };
+
+  render() {
+    return (
+        <div>
+          <div className="calculator-body">
+            <h1>Kalkulator Soal 5</h1>
+            <ResultComponent result={this.state.result}/>
+            <KeyPadComponent onClick={this.onClick}/>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
--- src/components/KeyPadComponent.js
+++ src/components/KeyPadComponent.js
@@ -7,7 +7,7 @@ class KeyPadComponent extends Component {
             <div className="button">
                 <button name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
                 <button name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
-                <button name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
+                <button name="%" onClick={e => this.props.onClick(e.target.name)}>%</button>
                 <button name="C" onClick={e => this.props.onClick(e.target.name)}>C</button><br/>
 
 
